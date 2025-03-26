e3f9544b8cc80dd70f3c9e1373911da420960987
David <108228801+David-Sonne@users.noreply.github.com>
2025-03-26 16:06:39 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Bauernhof bio = new Bauernhof(4, 5);
+        Saat[][] history = {
+                {Saat.Kartoffeln, Saat.Kartoffeln, Saat.Kartoffeln, Saat.Kartoffeln, Saat.Kartoffeln},
+                {Saat.Raps, Saat.Raps, Saat.Raps, Saat.Raps, Saat.Raps},
+                {Saat.Reis, Saat.Reis, Saat.Reis, Saat.Reis, Saat.Reis},
+                {Saat.Tomaten, Saat.Tomaten, Saat.Tomaten, Saat.Tomaten, Saat.Tomaten}
+        };
+        Bauernhof bio = new Bauernhof(4, 5, history);
+        bio.toString();
     }
 }
